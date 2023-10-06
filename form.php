4f3e3bdc2243854b843e5c2990c84896884371b0
Laura WOLFF <laura.wolff@live.fr>
2023-10-06 12:33:13 +0200

    added security server side

--- form.php
+++ form.php
@@ -3,24 +3,24 @@
 <form  action="thanks.php"  method="post">
     <div>
       <label  for="nom">Nom :</label>
-      <input  type="text"  id="nom"  name="user_firstname">
+      <input  type="text"  id="nom"  name="user_firstname" required>
     </div>
     <div>
       <label  for="nom-de-famille">Nom de famille:</label>
-      <input  type="text"  id="nom-de-famille"  name="user_lastname">
+      <input  type="text"  id="nom-de-famille"  name="user_lastname" required>
     </div>
     <div>
       <label  for="phone_number">Téléphone :</label>
-      <input  type="text"  id="phone"  name="user_phone_number">
+      <input  type="text"  id="phone"  name="user_phone_number" required>
     </div>
     <div>
       <label  for="courriel">Courriel :</label>
-        <input  type="email"  id="courriel"  name="user_email">
+        <input  type="email"  id="courriel"  name="user_email" required>
     </div>
     <div>
-      <label  for="courriel">Courriel :</label>
-        <select id="message_subject" name="message_subject">
-          <option value="Veuillez sélectionner">Veuillez sélectionner...</option>
+      <label  for="courriel">Objet du message :</label>
+        <select id="message_subject" name="message_subject" required>
+          <option value="">Veuillez sélectionner...</option>
           <option value="Effectuer un retour">Effectuer un retour</option>
           <option value="Suivi Livraison">Suivi Livraison</option>
           <option value="Erreur sur la commande">Erreur sur la commande</option>
@@ -29,9 +29,11 @@
     </div>
     <div>
       <label  for="message">Message :</label>
-      <textarea  id="message"  name="user_message"></textarea>
+      <textarea  id="message"  name="user_message" required></textarea>
     </div>
     <div  class="button">
       <button  type="submit">Envoyer votre message</button>
     </div>
   </form>
+
+  
--- thanks.php
+++ thanks.php
@@ -1,11 +1,44 @@
 <?php
 
-var_dump($_POST);
+$errors = [];
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+  if (!isset($_POST['user_firstname']) || empty($_POST['user_firstname'])) {
+    $errors[] = "Le prénom est obligatoire";
+  }
+  if (!isset($_POST['user_lastname']) || empty($_POST['user_lastname'])) {
+    $errors[] = "Le nom est obligatoire";
+  } 
+  if (!isset($_POST['user_phone_number']) || empty($_POST['user_phone_number'])) {
+    $errors[] = "Le numéro de téléphone est obligatoire";
+  } 
+  if (!isset($_POST['message_subject']) || empty($_POST['message_subject'])) {
+    $errors[] = "L'objet du message est obligatoire"; 
+  }  
+  if (!isset($_POST['user_email']) || empty($_POST['user_email'])) {
+    $errors[] = "L'e-mail est obligatoire"; 
+  }  
+  if (!isset($_POST['user_message']) || empty($_POST['user_message'])) {
+    $errors[] = "Le message est obligatoire"; 
+  }  
+}
 
 ?>
 
+<?php if ( count($errors) > 0 ) : ?>
+ <ul>
+    <?php foreach($errors as $error): ?>
+    <li><?= $error ?></li>
+    <?php endforeach; ?>
+ </ul>
+
+ <?php else : ?>
 <P>Merci <?= $_POST['user_firstname'].' '.$_POST['user_lastname']?> de nous avoir contacté à propos de “<?=$_POST['message_subject']?>”.</p>
 <br>
 <p>Un de nos conseillers vous contactera soit à l’adresse <?=$_POST['user_email']?> ou par téléphone au <?=$_POST['user_phone_number']?> dans les plus brefs délais pour traiter votre demande : </p>
 <br>
-<p><?=$_POST['user_message']?>
+<p><?=$_POST['user_message']?></p>
+
+<?php endif; ?>
